0a84d34d9fd5d70c47f95b5b96fc4bac627078d8
Niklas Emevi <niklas.mortensen@meridium.se>
2019-04-16 15:12:41 +0200

    initial commit of the url slug

---
+++ UrlSlug/src/index.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { TextInput, ValidationMessage } from '@contentful/forma-36-react-components';
+import { init } from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import speakingurl from 'speakingurl'
+import './index.css';
+
+class App extends React.Component {
+  static propTypes = {
+    sdk: PropTypes.object.isRequired,
+  };
+
+  detachExternalChangeHandler = null;
+  detachTitleValueChangeHandler = null;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.sdk.field.getValue(),
+      fieldId: props.sdk.field.id,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    this.props.sdk.window.startAutoResizer();
+
+    //handler for titlevalue change
+    var titleFieldName = this.props.sdk.contentType.displayField
+    var titleField = this.props.sdk.entry.fields[titleFieldName]
+    this.detachTitleValueChangeHandler = titleField.onValueChanged(
+      this.setValue
+    );
+
+    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
+      this.onExternalChange
+    );
+  }
+
+  componentWillUnmount() {
+    if (this.detachExternalChangeHandler) {
+      this.detachExternalChangeHandler();
+    }
+    if(this.detachTitleValueChangeHandler){
+      this.detachTitleValueChangeHandler();
+    }
+  }
+
+  slugify = value => speakingurl(value,{
+    custom:{
+      'Å':"a", 'Ä':"a",
+      'Ö':"o", 'å':"a",
+      'ä':"a", 'ö':"o",
+    }
+  })
+
+  validateUniqueness = value => {
+    var query = {}
+    query['content_type'] = this.props.sdk.entry.getSys().contentType.sys.id
+    query['fields.' + this.state.fieldId] = value
+    query['sys.id[ne]'] = this.props.sdk.entry.getSys().id
+    query['sys.publishedAt[exists]'] = true
+    return this.props.sdk.space.getEntries(query).then(function (result) {
+      if(!value) return false;
+      return result.total > 0
+    })
+  }
+
+  setValue = value => {
+    value = this.slugify(value);
+
+    this.setState({
+      value
+    })
+
+    this.validateUniqueness(value).then(hasDuplicates => {
+      if(hasDuplicates){
+        this.setState({
+          error: "Det finns redan ett innehåll med detta url-segment",
+        })
+        return;
+      }
+      this.setState({ 
+        error: null
+      });
+      if (value) {
+        this.props.sdk.field.setValue(value);
+      } else {
+        this.props.sdk.field.removeValue();
+      }
+    });
+  }
+
+
+  onExternalChange = value => {
+    if(value !== this.state.value){
+      this.setState({ value });
+    }
+  };
+
+  onChange = e => {
+    this.setValue(e.currentTarget.value)
+  };
+
+  render() {
+    return (<>
+      <TextInput
+        type="text"
+        value={this.state.value}
+        onChange={this.onChange}
+        name="urlslug"
+        id=""
+      />
+      {this.state.error ? <ValidationMessage>{this.state.error}</ValidationMessage> : null}
+      </>
+    );
+  }
+}
+
+init(sdk => {
+  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+});
+
+/**
+ * By default, iframe of the extension is fully reloaded on every save of a source file.
+ * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
+ */
+// if (module.hot) {
+//   module.hot.accept();
+// }
