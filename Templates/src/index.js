82052083ae29c57606a3a3ee3a5456ef2fe27e12
Emil Larsson <emil.larsson@tieto.com>
2019-09-05 15:22:01 +0200

    Get preset and replace values

--- Templates/src/index.js
+++ Templates/src/index.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { TextInput, ValidationMessage } from '@contentful/forma-36-react-components';
+import { SelectField, Option, Button } from '@contentful/forma-36-react-components';
 import { init } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
-// import speakingurl from 'speakingurl'
 import './index.css';
+import Promise from 'bluebird'
 
 class App extends React.Component {
-  // static propTypes = {
-  //   sdk: PropTypes.object.isRequired,
-  // };
-
-  detachExternalChangeHandler = null;
-  detachTitleValueChangeHandler = null;
-  inputTimeoutDebounce = null
 
   constructor(props) {
     super(props);
-    console.log('wohoo')
-    console.log(props)
 
     this.state = {
-      // value: props.sdk.field.getValue(),
-      // initialValue: props.sdk.field.getValue(),
-      error: null
+      api: props.sdk,
     };
   }
 
@@ -35,22 +24,70 @@ class App extends React.Component {
   componentWillUnmount() {
   }
 
+  getPresetAndReplaceValues () {
+    const contentTypeName = this.state.api.contentType.name
+    this.state.api.space.getEntries({
+      content_type: contentTypeName,
+      'query':'[mall]'
+    }).then(data => {
+      const arr = []
+      Object.entries(data.items[0].fields).map(([key, value]) => {
+        arr.push({ key: key, value: value})
+      })
+
+      return Promise.each(arr, obj => {
+        return new Promise((resolve, reject) => {
+          const { key, value } = obj
+          if (this.state.api.entry.fields[key].getValue() !== undefined) {
+              this.showDialog(key)
+              .then(result => {
+                if (result) {
+                  this.state.api.entry.fields[key].setValue(this.removeMallFromString(value['en-US']))
+                  console.log('satte nytt varde')
+                }
+                resolve()
+              })
+          } else {
+            this.state.api.entry.fields[key].setValue(this.removeMallFromString(value['en-US']))
+            resolve()
+          }
+        })
+      })
+    }).then(data => console.log(data))
+    .catch(e => console.log(e))
+  }
+
+  showDialog(key) {
+    return this.state.api.dialogs.openConfirm({
+        title: 'Värdet redan ifyllt för ' + key,
+        message: 'Är du säker på att du vill skriva över?',
+        intent: 'positive',
+        confirmLabel: 'Ja',
+        cancelLabel: 'Nej'
+      }).then(result => result)
+      .catch(e => console.log(e))
+  }
+
+  removeMallFromString(str) {
+    let strFixed = str.replace('[mall] ', '')
+    strFixed = strFixed.replace('[mall]', '')
+    return strFixed
+  }
+
   render() {
     return (<>
-      <p>hmmm</p>
-      {/* <TextInput
-        type="text"
-        value={this.state.value}
-        onChange={this.onChange}
-        name="urlslug"
-        id="urlslug"
-      />
-      {this.state.error ? <ValidationMessage>{this.state.error}</ValidationMessage> : null} */}
-      </>
-    );
+      <Button
+        style={{ marginTop: '15px' }}
+        buttonType='primary'
+        onClick={() => {
+          this.getPresetAndReplaceValues()
+        }}
+      >Hämta standardvärden</Button>
+    </>
+    )
   }
 }
 
 init(sdk => {
   ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
-});
+})
