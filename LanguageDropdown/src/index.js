379c008f1cbaaa4298d6d6efea1aa9c6dd3721b4
Niklas Emevi <niklas.mortensen@meridium.se>
2020-02-24 13:34:21 +0100

    adding supported lang dropdown

---
+++ LanguageDropdown/src/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { init } from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import './index.css';
+
+const supportedLangs = {
+  EN :"Engelska",
+  AR:"Arabiska",
+  ES:"Spanska"  ,
+  FI:"Finska",
+  TR:"Turkiska",
+  "BS/HR/SR":"Bosniska/Kroatiska/Serbiska",
+  KU :"Nordkurdiska (latinskt) (Kurmancî)",
+  SO:"Somaliska",
+  SMI:"Samiska",
+  KU :"Sorani",
+  ROM:"Romani",
+  FIU:"Meänkieli",
+  TI:"Tigrinska",
+  YI:"Jiddisch"
+}
+
+export class App extends React.Component {
+  static propTypes = {
+    sdk: PropTypes.object.isRequired,
+    
+  };
+
+  detachExternalChangeHandler = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.sdk.field.getValue() || '',
+      isOpen: false
+    };
+  }
+
+  componentDidMount() {
+    this.props.sdk.window.startAutoResizer();
+
+    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
+  }
+
+  componentWillUnmount() {
+    if (this.detachExternalChangeHandler) {
+      this.detachExternalChangeHandler();
+    }
+  }
+
+  onExternalChange = value => {
+    this.setState({ value });
+  };
+
+  onChange = e => {
+    const value = e.currentTarget.value;
+    this.setState({ value });
+    if (value) {
+      this.props.sdk.field.setValue(value);
+    } else {
+      this.props.sdk.field.removeValue();
+    }
+  };
+
+  render() {
+    return <select onChange={this.onChange.bind(this)}>
+      <option>Välj ett språk</option>
+      { Object.keys(supportedLangs).map(x => <option key={x} value={x}>{supportedLangs[x]}</option>) }
+
+    </select>
+    
+  }
+}
+
+init(sdk => {
+  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+});
+
+/**
+ * By default, iframe of the extension is fully reloaded on every save of a source file.
+ * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
+ */
+// if (module.hot) {
+//   module.hot.accept();
+// }
